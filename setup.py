0c9df6d6b1bb203e912f30b1a39474e6d954158b
Kilel15 <sharonkilel15@gmail.com>
2025-03-22 07:25:26 -0700

    Added setup.py for installable package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mathutils",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    description="A simple Python math utilities package",
+    author="Your Name",
+    author_email="your_email@example.com",
+    license="MIT",
+)
